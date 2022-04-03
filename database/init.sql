c636e7e7e66b1047c6dbf34befb26fb5634c25da
George Joubran <durd2001@gmail.com>
2022-04-03 23:26:51 +0300

    working on posts

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users,friends,messages,requests,rooms,participants CASCADE;
+DROP TABLE IF EXISTS users,friends,messages,requests,rooms,participants,posts,comments CASCADE;
 
 CREATE TABLE users (
     userid SERIAL PRIMARY KEY,
@@ -51,6 +51,24 @@ CREATE TABLE messages (
     message text
 );
 
+CREATE TABLE posts(
+    postid SERIAL PRIMARY KEY,
+    userid INTEGER REFERENCES users(userid),
+    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    post_title varchar(55),
+    post_content text,
+    likes INTEGER
+);
+
+CREATE TABLE comments(
+    commentid SERIAL PRIMARY KEY,
+    postid INTEGER REFERENCES posts(postid),
+    userid INTEGER REFERENCES users(userid),
+    parentid INTEGER REFERENCES comments(commentid),
+    comment_content text,
+    likes INTEGER
+);
+
 INSERT INTO users (username,password,email,firstname,lastname,age,gender,imageid) VALUES
 ('Kuala1','$2a$10$4d0ons8ELFLe4Blw67vVMujjLefWnmFfB7tjz.ZLOw9CC2ywS.aNa','durd20011@gmail.com','George','Jobran',20,'Male','mariosticker'),
 ('Mario','$2a$10$4d0ons8ELFLe4Blw67vVMujjLefWnmFfB7tjz.ZLOw9CC2ywS.aNa','durd20021@gmail.com','Mario','Saliba',20,'Male','mariosticker'),
@@ -89,5 +107,12 @@ INSERT INTO participants (userid,roomid) VALUES
 INSERT INTO messages (userid,roomid,message) VALUES
 (1,1,'Hello, my name is George, Its nice to meet you!'),
 (2,1,'Hello there George, My name is Mario, Pleasured to meet you!');
+
+INSERT INTO posts(postid,userid,post_title,post_content,likes) VALUES
+(1,1,'Mario sucks ass','Mario actuallly sucks big ass and i love him for that',69);
+
+INSERT INTO comments(commentid,postid,userid,parentid,comment_content,likes) VALUES
+(1,1,2,null,'yea he does suck lol',21),
+(2,1,3,1,'Hey dont you talk about mario like that',-5);
 
 COMMIT;
